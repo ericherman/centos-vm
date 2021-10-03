8976eb4fcca84d88ee58c1c49a7ff19d12dcd4a7
Eric Herman <eric@freesa.org>
2021-10-03 18:45:27 +0200

    pull in the newer retry

--- retry.sh
+++ retry.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-retry_count=$1
+max_retry_count=$1
 shift
 
 sleep_time=$1
 shift
 
+retry_count=$max_retry_count
 exit_code=1
+
+# ensure log file exists
+touch .retry.log
+
 while [ $retry_count -ge 0 ]; do
-	$@
+	# keep only the most recent ~1000 lines
+	tail --lines=1000 .retry.log > .retry.log
+	$@ >> .retry.log 2>&1
 	exit_code=$?
 	if [ $exit_code -eq 0 ]; then
 		exit 0;
 	fi
 	retry_count=$(($retry_count - 1))
 	if [ $retry_count -ge 0 ]; then
-		echo "will retry ($retry_count) in $sleep_time second(s)"
+		printf "\rretrying failed command in $sleep_time second(s) "
+		printf "($retry_count of $max_retry_count retries left)...   "
 		sleep $sleep_time
 	else
 		echo "will not retry"
